4fdc2234260227b7c4e26f4f7e0f4833709527e7
gowsalya446 <71876439+gowsalya446@users.noreply.github.com>
2024-09-20 12:30:41 +0530

    Feat:Initial FrontEnd commit

---
+++ simpletaskFrontend/src/components/completedTask/index.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react';
+export default function Completedtask(){
+   
+
+    
+      const [tasks, setTasks] = useState([]); 
+      const [loading, setLoading] = useState(true); 
+      const [error, setError] = useState(null); 
+    
+      useEffect(() => {
+        const fetchTasks = async () => {
+          try {
+            const response = await fetch('http://127.0.0.1:8000/api/task/');
+            if (!response.ok) {
+              throw new Error('Failed to fetch tasks');
+            }
+            const data = await response.json();
+            setTasks(data); 
+          } catch (err) {
+            setError(err.message);
+          } finally {
+            setLoading(false);
+          }
+        };
+    
+        fetchTasks();
+      }, []); 
+    
+      
+      const completedTasks = tasks.filter(task => task.is_completed);
+    
+      if (loading) return <div>Loading...</div>;
+      if (error) return <div>Error: {error}</div>;
+    
+      return (
+        <div className="table-container">
+      <h2 className="table-title">Completed List</h2>
+      <table className="task-table">
+        <thead className="task-table-header">
+          <tr>
+            <th className="task-table-header-cell">ID</th>
+            <th className="task-table-header-cell">Title</th>
+            <th className="task-table-header-cell">Task</th>
+            <th className="task-table-header-cell">Completed</th>
+            
+
+
+          </tr>
+        </thead>
+        <tbody className="task-table-body">
+          {completedTasks.map((task,index) => (
+            <tr key={task.id} className="task-table-row">
+              <td className="task-table-cell">{index + 1}</td>
+              <td className="task-table-cell">{task.title}</td>
+              <td className="task-table-cell">{task.task}</td>
+              <td className="task-table-cell">
+                <input
+                  type="checkbox"
+                  checked={task.is_completed}
+                 
+                  className="task-checkbox"
+                />
+              </td>
+
+            </tr>
+          ))}
+        </tbody>
+      </table>
+     
+    </div>
+      );
+    };
+    
+    
+    
